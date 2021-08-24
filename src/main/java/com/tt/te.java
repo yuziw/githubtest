5e3f12b9af3679f54744664795884189cd5fb924
yu <107491002@example.com>
2021-08-24 13:03:39 +0800

    1.0

---
+++ src/main/java/com/tt/te.java
@@ -0,0 +1,11 @@
+package com.tt;
+
+/**
+ * @author Lurenc
+ * @create 2021-08-24 12:57
+ */
+public class te {
+    public static void main(String[] args) {
+        System.out.println(1111);
+    }
+}
---
+++ src/test/java/com/test.java
@@ -0,0 +1,10 @@
+package com;
+
+/**
+ * @author Lurenc
+ * @create 2021-08-24 12:54
+ */
+public class test {
+
+
+}
